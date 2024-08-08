d98c3b9b078ad79ebf5f369e1a1ac56f2809c5b5
GMT <giullianomteixeira@gmail.com>
2024-08-08 16:44:05 -0300

    Padronizações, implementação em inglês

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ import tempfile
 import json
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+       filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+           
+@app.route('/index', methods=['GET'])
+def index_html():
+    return render_template('index.html')
+
+@app.route('/inserir_info_pt', methods=['GET'])
+def info_pt():
+    return render_template('inserir_info_pt.html')
+
+@app.route('/respostas_pt', methods=['GET'])
+def respostas_pt():
+    return render_template('respostas_pt.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,35 +28,44 @@ def index():
     image_urls = {}
 
     if request.method == 'POST':
-        # Create a temporary directory for this request
+        # Creating a temporary directory for this request
         temp_dir = tempfile.mkdtemp()
 
-        # Handle text data
-        form_data['nome_maquina'] = request.form.get('nomeMaquina')
-        form_data['tipo_maquina'] = request.form.get('tipoMaquina')
+        # Handling text data
+        form_data['nome_maquina'] = request.form.get('machineName')
+        form_data['tipo_maquina'] = request.form.get('machineType')
 
-        # Handle image uploads
+        # Handling image uploads
         for key in request.files:
             file = request.files[key]
             if file and allowed_file(file.filename):
                 filename = file.filename
                 filepath = os.path.join(temp_dir, filename)
                 file.save(filepath)
-                image_urls[key] = filename  # Store image filenames
+                image_urls[key] = filename  # Storing image filenames
 
         json_data = {
             'form_data': form_data,
             'image_urls': image_urls
         }
         
-        with open('data.json', 'w') as json_file:
+        with open('user_info.json', 'w') as json_file:
             json.dump(json_data, json_file)
 
         return jsonify(json_data)
 
     return render_template('index.html')
 
+@app.route('/data_pt', methods=['GET'])
+def get_data_pt(json_data):
+    data = chatGPT_response_PT(json_data)
+    return jsonify(data)
+
+
+@app.route('/data_en', methods=['GET'])
+def get_data_en(json_data):
+    data = chatGPT_response_EN(json_data)
+    return jsonify(data)
+
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(host='127.0.0.1', port=5000)
+    app.run(debug=True)
