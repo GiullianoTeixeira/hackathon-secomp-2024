0f8870cb35b4c68bd88b64d90a12d9108429ee39
GMT <giullianomteixeira@gmail.com>
2024-08-08 14:21:54 -0300

    Avanços no Backend

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template, redirect, url_for
-import tempfile
+from flask import Flask, request, render_template, jsonify
 import os
-import shutil
+import tempfile
+import json
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
@@ -12,37 +13,39 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    form_data = {}
+    image_urls = {}
+
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            # Create a temporary directory for this request
-            temp_dir = tempfile.mkdtemp()
-            filename = file.filename
-            filepath = os.path.join(temp_dir, filename)
-            file.save(filepath)
-            
-            # Pass the temp_dir and filename to the template
-            return redirect(url_for('index', filename=filename, temp_dir=temp_dir))
-        else:
-            return redirect(request.url)
-    
-    filename = request.args.get('filename')
-    temp_dir = request.args.get('temp_dir')
-    
-    # Render the template with the filename and temp_dir
-    if filename and temp_dir:
-        filepath = os.path.join(temp_dir, filename)
-        if os.path.exists(filepath):
-            # Clean up the temporary directory after use
-            os.remove(filepath)
-            shutil.rmtree(temp_dir)
-        return render_template('index.html', filename=filename, temp_dir=temp_dir)
-    
+        # Create a temporary directory for this request
+        temp_dir = tempfile.mkdtemp()
+
+        # Handle text data
+        form_data['nome_maquina'] = request.form.get('nomeMaquina')
+        form_data['tipo_maquina'] = request.form.get('tipoMaquina')
+
+        # Handle image uploads
+        for key in request.files:
+            file = request.files[key]
+            if file and allowed_file(file.filename):
+                filename = file.filename
+                filepath = os.path.join(temp_dir, filename)
+                file.save(filepath)
+                image_urls[key] = filename  # Store image filenames
+
+        json_data = {
+            'form_data': form_data,
+            'image_urls': image_urls
+        }
+        
+        with open('data.json', 'w') as json_file:
+            json.dump(json_data, json_file)
+
+        return jsonify(json_data)
+
     return render_template('index.html')
 
 if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
